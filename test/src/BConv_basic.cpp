22a152e8c3046b1fe169dbbf17a3b60c6d2acbd2
ryantran16 <ryantran.rs@gmail.com>
2019-11-23 21:02:17 -0800

    bleh

--- test/src/BConv_basic.cpp
+++ test/src/BConv_basic.cpp
@@ -38,12 +38,11 @@ int test_forward_t() {
 
     float out[((n - k) + 1) * ((m - k) + 1)];
     Halide::Buffer<float> out_buf(res, ((m - k) + 1), ((n - k) + 1), "out_buf");
-    printf("apple %d %d %d %d",
-        res_buf.dim(0).extent(),
-        res_buf.dim(1).extent(),
-        out_buf.dim(0).extent(),
-        out_buf.dim(1).extent()
-    )
+    std::cout << 
+        res_buf.dim(0).extent() << " " <<
+        res_buf.dim(1).extent() << " " <<
+        out_buf.dim(0).extent() << " " <<
+        out_buf.dim(1).extent() << std::endl;
     dut.forward_t(&out_buf, res_buf);
     delete res;
     return 0;
