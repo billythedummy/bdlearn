84b5c2188c812805e707fbda418e812638a39b3a
ryantran16 <ryantran.rs@gmail.com>
2019-11-23 21:11:52 -0800

    bleh

--- src/core/BConvLayer.cpp
+++ src/core/BConvLayer.cpp
@@ -13,8 +13,13 @@ namespace bdlearn {
         // TO-DO
         int cols = in.dim(0).extent();
         int rows = in.dim(1).extent();
+        const int out_height = (rows - k_) / 1 + 1;
+        const int out_width = (cols - k_) / 1 + 1;
+        const int patch_area = k_* k_;
+        const int h_im2col = patch_area * in_c_;
+        const int w_im2col = out_height * out_width;
         float* in_im2col = BConvLayer::im2col<float>(in, rows, cols, in_c_, 0, 0, s_, s_, k_, k_);
-        BMat mat_in(rows, cols, in_im2col);
+        BMat mat_in(h_im2col, w_im2col, in_im2col);
         delete in_im2col;
         std::cout << "im2col" << mat_in.rows() << " " << mat_in.cols() << std::endl;
         matmul(out, w_, mat_in);
