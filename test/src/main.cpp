850280f975732df762cc0631ff8857fc386293f3
dhy1996 <dhy1996@live.com.sg>
2019-12-02 18:00:22 -0800

    append_gap moedl

--- test/src/main.cpp
+++ test/src/main.cpp
@@ -28,8 +28,8 @@ int main(int argc, char **argv) {
     //if (test_BConv_forward_backward()) return -1;
     //if (test_Model()) return -1;
     //if (test_softmax()) return -1;
-    if (test_GAP_forward_backward_t()) return -1;
-    //if (test_Ensemble()) return -1;
+    //if (test_GAP_forward_backward_t()) return -1;
+    if (test_Ensemble()) return -1;
     //if (test_Model()) return -1;
     std::cout << "All tests passed!" << std::endl;
     return 0;
