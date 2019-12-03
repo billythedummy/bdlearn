fe4e6f5753eab7735a53f86989b2058ec86b9026
ryantran16 <ryantran.rs@gmail.com>
2019-12-03 00:54:12 -0800

    added save-load for bconv?

--- src/core/BConvLayer.cpp
+++ src/core/BConvLayer.cpp
@@ -165,7 +165,7 @@ namespace bdlearn {
         std::istringstream s(line);
         for (int i = 0; i < size_; ++i) {
             std::getline(s, data, ',');
-            data = "1000";
+            std::cout << data << std::endl;
             train_w_.get()[i] = std::stof(data, &sz);
         }
         w_.sign(train_w_.get());
