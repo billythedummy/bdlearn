b6bbc55938f9a196683fd7b92a8cbe34c9b83da3
ryantran16 <ryantran.rs@gmail.com>
2019-12-02 20:41:11 -0800

    added prints

--- src/core/BConvLayer.cpp
+++ src/core/BConvLayer.cpp
@@ -67,7 +67,7 @@ namespace bdlearn {
         BMat in_mat(h_im2col, w_im2col, in_im2col);
         // Matmul weights with BMat
         std::cout << "in 0:" << in_im2col_view.dim(0).extent() << " 1:" << in_im2col_view.dim(1).extent() << std::endl;
-        std::cout << "out 0:" << out.dim(0).extent() << " 1:" << out.dim(1).extent() << std::endl;
+        std::cout << "out 0:" << out.dim(0).extent() << " 1:" << out.dim(1).extent() << " 2:" << out.dim(2).extent() << std::endl;
         std::cout << "in_mat 0:" << in_mat.rows() << " 1:" << in_mat.cols() << std::endl;
         std::cout << "w 0:" << w_.rows() << " 1:" << w_.cols() << std::endl;
         matmul(out, w_, in_mat);
