9d0a49e5417e3c954bc5f0dac7ecf7f59cdd222f
Justin La <lajustin99@gmail.com>
2019-12-03 19:45:38 -0800

    set up testing for max pool

---
+++ test/src/CMakeFiles/bdlearn_test.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/homes/iws/jula99/455/bdlearn/test/src/BMat_basic.cpp" "/homes/iws/jula99/455/bdlearn/test/src/CMakeFiles/bdlearn_test.dir/BMat_basic.o"
+  "/homes/iws/jula99/455/bdlearn/test/src/Halide_test.cpp" "/homes/iws/jula99/455/bdlearn/test/src/CMakeFiles/bdlearn_test.dir/Halide_test.o"
+  "/homes/iws/jula99/455/bdlearn/test/src/main.cpp" "/homes/iws/jula99/455/bdlearn/test/src/CMakeFiles/bdlearn_test.dir/main.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ test/src/CMakeFiles/bdlearn_test.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/bdlearn_test.dir/main.o"
+  "CMakeFiles/bdlearn_test.dir/BMat_basic.o"
+  "CMakeFiles/bdlearn_test.dir/Halide_test.o"
+  "bdlearn_test.pdb"
+  "bdlearn_test"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/bdlearn_test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
